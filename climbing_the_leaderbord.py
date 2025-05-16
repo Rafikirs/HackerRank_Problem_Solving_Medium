0e51f7e560a282de02081ca34ef985724402ba0d
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 13:53:52 +0200

    Update climbing_the_leaderbord.py

--- climbing_the_leaderbord.py
+++ climbing_the_leaderbord.py
@@ -1,3 +1,11 @@
+# Exercise: Climbing the Leaderbord
+# URL: https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem?isFullScreen=true
+# Description:
+# A player wants to track their rank after each game on a dense ranking leaderboard. In dense ranking:
+# - The player with the highest score is rank 1.
+# - Equal scores share the same rank.
+# - The next unique score gets the next rank (no skipping).
+
 def climbingLeaderboard(ranked, player):
     unique_ranked = sorted(set(ranked), reverse=True)
     
